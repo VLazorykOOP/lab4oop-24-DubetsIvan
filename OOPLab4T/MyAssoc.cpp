60a99882db03baa55099551ef1a910b4404d62e4
DubetsIvan <158566315+DubetsIvan@users.noreply.github.com>
2024-05-08 20:28:12 +0300

    .

--- OOPLab4T/MyAssoc.cpp
+++ OOPLab4T/MyAssoc.cpp
@@ -1,24 +1,107 @@
-#include "MyAssoc.h"
-ostream& operator<<(ostream& os, MyTime& t) {
-	os << t.hour << ":";
-	if (t.minute <= 9) os << "0" << t.minute; else os << t.minute;
-	os << " ";
-	return os;
-}
-istream& operator>>(istream& iso, MyTime& t) {
-	iso >> t.hour >> t.minute;
-	t.hour %= 24;
-	t.minute %= 60;
-	return iso;
-}
+#include <iostream>
+#include <string>
 
-void MyAssoc::TableFlight()
-{
-		cout << " TableFlight \n";
-		for (int i = 0; i < numRec; i++) {
-			cout << " Flight " << MasFlightNumber[i] << " time " 
-				 << MasTime[i] << endl;
-		}
+class Books {
+    struct Node {
+        std::string ID_book;
+        std::string name;
+        Node* next;
+
+        Node(const std::string& e, const std::string& p) : ID_book(e), name(p), next(nullptr) {}
+    };
+
+    Node* head;
+    int errorCode;
+
+public:
+    enum ErrorCodes {
+        NO_ERROR = 0,
+        EMAIL_NOT_FOUND
+    };
+
+    Books() : head(nullptr), errorCode(NO_ERROR) {}
+
+    ~Books() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void createAssociations(const std::string& email, const std::string& phone) {
+        Node* newNode = new Node(email, phone);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    std::string operator[](const std::string& email) {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->ID_book == email) {
+                errorCode = NO_ERROR;
+                return current->name;
+            }
+            current = current->next;
+        }
+        errorCode = EMAIL_NOT_FOUND;
+        return "";
+    }
+
+    std::string operator()(const std::string& email) {
+        return (*this)[email];
+    }
+
+    int getErrorCode() const {
+        return errorCode;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Books& assoc) {
+        Node* current = assoc.head;
+        while (current != nullptr) {
+            os << "Email: " << current->ID_book << ", Phone: " << current->name << std::endl;
+            current = current->next;
+        }
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Books& assoc) {
+        std::string email, phone;
+        while (is >> email >> phone) {
+            assoc.createAssociations(email, phone);
+        }
+        return is;
+    }
+};
+
+
+int main() {
+    Books books;
+
+    books.createAssociations("biba@gmail.com", "0951234567");
+    books.createAssociations("boba@gmail.com", "0501234567");
+
+    std::cout << "Books:" << std::endl;
+    std::cout << books << std::endl;
+
+    std::string emailToFind = "boba@gmail.com";
+    std::string phone = books[emailToFind];
+    if (books.getErrorCode() == Books::NO_ERROR) {
+        std::cout << "Phone for email " << emailToFind << ": " << phone << std::endl;
+    }
+    else {
+        std::cout << "Email " << emailToFind << " not found!" << std::endl;
+    }
+
+    std::string emailToFind2 = "biba@gmail.com";
+    std::string phone2 = books(emailToFind2);
+    if (!phone2.empty()) {
+        std::cout << "Phone for email " << emailToFind2 << ": " << phone2 << std::endl;
+    }
+    else {
+        std::cout << "Email " << emailToFind << " not found!" << std::endl;
+    }
+
+    return 0;
 }
-	
 
--- OOPLab4T/OOPLab4T.cpp
+++ OOPLab4T/OOPLab4T.cpp
@@ -1,24 +1,440 @@
-﻿ // OOPLab4T.cpp : Цей файл містить є шаблоном для 
-// виконання лаб. роботи №4. 
-// Він містинь функцію "main" з якої починається та закінчується виконання програми.
-//
+﻿#include <iostream>
 
-
-#include <iostream>
-
-// Ваші файли загловки 
-//
-#include "Lab4Exmaple.h"
-int main()
+class VectorULong
 {
-    std::cout << " Lab #4  task chain  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+    static int objectCount;
+    unsigned long* a;
+    int size;
+    int state;
 
-    int chain = 1;
-    if (chain == 1) chain = mainExample1();
-    if (chain == 2) chain = mainExample2();
-    if (chain == 3) chain = mainExample3();
+public:
+    enum State {
+        NO_ERROR = 0,
+        OUT_OF_MEMORY,
+        INDEX_OUT_OF_RANGE
+    };
 
+    int getErrorCode() const {
+        return state;
+    }
+
+    static int getObjectCount() {
+        return objectCount;
+    }
+
+    VectorULong() : size(1), state(NO_ERROR) {
+        a = new unsigned long[size];
+        if (a == nullptr) {
+            state = OUT_OF_MEMORY;
+            return;
+        }
+        a[0] = 0;
+        ++objectCount;
+    }
+
+    VectorULong(int Size) : size(Size), state(NO_ERROR) {
+        a = new unsigned long[Size];
+        if (a == nullptr) {
+            state = OUT_OF_MEMORY;
+            return;
+        }
+        for (int i = 0; i < Size; i++) {
+            a[i] = 0;
+        }
+        ++objectCount;
+    }
+
+    VectorULong(int Size, unsigned long Value) : size(Size), state(NO_ERROR) {
+        a = new unsigned long[Size];
+        if (a == nullptr) {
+            state = OUT_OF_MEMORY;
+            return;
+        }
+        for (int i = 0; i < Size; i++) {
+            a[i] = Value;
+        }
+        ++objectCount;
+    }
+
+    VectorULong(const VectorULong& other) : size(other.size), state(other.state) {
+        a = new unsigned long[size];
+        if (a == nullptr) {
+            state = OUT_OF_MEMORY;
+            return;
+        }
+        for (int i = 0; i < size; i++) {
+            a[i] = other.a[i];
+        }
+        ++objectCount;
+    }
+
+    ~VectorULong() {
+        delete[] a;
+        --objectCount;
+    }
+
+    VectorULong& operator=(const VectorULong& other) {
+        if (this != &other) {
+            delete[] a;
+            size = other.size;
+            state = other.state;
+            a = new unsigned long[size];
+            if (a == nullptr) {
+                state = OUT_OF_MEMORY;
+                return *this;
+            }
+            for (int i = 0; i < size; i++) {
+                a[i] = other.a[i];
+            }
+        }
+        return *this;
+    }
+
+    VectorULong operator+(const VectorULong& b) const {
+        if (size != b.size) {
+            VectorULong result(1);
+            result.state = INDEX_OUT_OF_RANGE;
+            return result;
+        }
+
+        VectorULong result(size);
+        for (int i = 0; i < size; i++) {
+            result.a[i] = a[i] + b.a[i];
+        }
+        return result;
+    }
+
+    VectorULong operator-(const VectorULong& b) const {
+        if (size != b.size) {
+            VectorULong result(1);
+            result.state = INDEX_OUT_OF_RANGE;
+            return result;
+        }
+
+        VectorULong result(size);
+        for (int i = 0; i < size; i++) {
+            result.a[i] = a[i] - b.a[i];
+        }
+        return result;
+    }
+
+    VectorULong operator*(unsigned long scalar) const {
+        VectorULong result(size);
+        for (int i = 0; i < size; i++) {
+            result.a[i] = a[i] * scalar;
+        }
+        return result;
+    }
+
+    VectorULong operator/(unsigned long scalar) const {
+        if (scalar == 0) {
+            VectorULong result(1);
+            result.state = INDEX_OUT_OF_RANGE;
+            return result;
+        }
+
+        VectorULong result(size);
+        for (int i = 0; i < size; i++) {
+            result.a[i] = a[i] / scalar;
+        }
+        return result;
+    }
+
+    VectorULong operator%(unsigned long scalar) const {
+        if (scalar == 0) {
+            VectorULong result(1);
+            result.state = INDEX_OUT_OF_RANGE;
+            return result;
+        }
+
+        VectorULong result(size);
+        for (int i = 0; i < size; i++) {
+            result.a[i] = a[i] % scalar;
+        }
+        return result;
+    }
+
+    VectorULong operator|(const VectorULong& b) const {
+        if (size != b.size) {
+            VectorULong result(1);
+            result.state = INDEX_OUT_OF_RANGE;
+            return result;
+        }
+
+        VectorULong result(size);
+        for (int i = 0; i < size; i++) {
+            result.a[i] = a[i] | b.a[i];
+        }
+        return result;
+    }
+
+    VectorULong operator^(const VectorULong& b) const {
+        if (size != b.size) {
+            VectorULong result(1);
+            result.state = INDEX_OUT_OF_RANGE;
+            return result;
+        }
+
+        VectorULong result(size);
+        for (int i = 0; i < size; i++) {
+            result.a[i] = a[i] ^ b.a[i];
+        }
+        return result;
+    }
+
+    VectorULong operator&(const VectorULong& b) const {
+        if (size != b.size) {
+            VectorULong result(1);
+            result.state = INDEX_OUT_OF_RANGE;
+            return result;
+        }
+
+        VectorULong result(size);
+        for (int i = 0; i < size; i++) {
+            result.a[i] = a[i] & b.a[i];
+        }
+        return result;
+    }
+
+    VectorULong operator++() {
+        for (int i = 0; i < size; i++) {
+            ++a[i];
+        }
+        return *this;
+    }
+
+    VectorULong operator++(int) {
+        VectorULong temp(*this);
+        operator++();
+        return temp;
+    }
+
+    VectorULong operator--() {
+        for (int i = 0; i < size; i++) {
+            --a[i];
+        }
+        return *this;
+    }
+
+    VectorULong operator--(int) {
+        VectorULong temp(*this);
+        operator--();
+        return temp;
+    }
+
+    VectorULong operator-() const {
+        VectorULong result(*this);
+        for (int i = 0; i < size; i++) {
+            result.a[i] - result.a[i];
+        }
+        return result;
+    }
+
+    bool operator!() const {
+        return size == 0;
+    }
+
+    VectorULong operator~() const {
+        VectorULong result(*this);
+        for (int i = 0; i < size; i++) {
+            result.a[i] = ~result.a[i];
+        }
+        return result;
+    }
+
+    VectorULong& operator+=(const VectorULong& b) {
+        if (size != b.size) {
+            state = INDEX_OUT_OF_RANGE;
+            return *this;
+        }
+
+        for (int i = 0; i < size; i++) {
+            a[i] += b.a[i];
+        }
+        return *this;
+    }
+
+    VectorULong& operator-=(const VectorULong& b) {
+        if (size != b.size) {
+            state = INDEX_OUT_OF_RANGE;
+            return *this;
+        }
+
+        for (int i = 0; i < size; i++) {
+            a[i] -= b.a[i];
+        }
+        return *this;
+    }
+
+    VectorULong& operator*=(unsigned long scalar) {
+        for (int i = 0; i < size; i++) {
+            a[i] *= scalar;
+        }
+        return *this;
+    }
+
+    VectorULong& operator/=(unsigned long scalar) {
+        if (scalar == 0) {
+            state = INDEX_OUT_OF_RANGE;
+            return *this;
+        }
+
+        for (int i = 0; i < size; i++) {
+            a[i] /= scalar;
+        }
+        return *this;
+    }
+
+    VectorULong& operator%=(unsigned long scalar) {
+        if (scalar == 0) {
+            state = INDEX_OUT_OF_RANGE;
+            return *this;
+        }
+
+        for (int i = 0; i < size; i++) {
+            a[i] %= scalar;
+        }
+        return *this;
+    }
+
+    VectorULong& operator|=(const VectorULong& b) {
+        if (size != b.size) {
+            state = INDEX_OUT_OF_RANGE;
+            return *this;
+        }
+
+        for (int i = 0; i < size; i++) {
+            a[i] |= b.a[i];
+        }
+        return *this;
+    }
+
+    VectorULong& operator^=(const VectorULong& b) {
+        if (size != b.size) {
+            state = INDEX_OUT_OF_RANGE;
+            return *this;
+        }
+
+        for (int i = 0; i < size; i++) {
+            a[i] ^= b.a[i];
+        }
+        return *this;
+    }
+
+    VectorULong& operator&=(const VectorULong& b) {
+        if (size != b.size) {
+            state = INDEX_OUT_OF_RANGE;
+            return *this;
+        }
+
+        for (int i = 0; i < size; i++) {
+            a[i] &= b.a[i];
+        }
+        return *this;
+    }
+
+    bool operator==(const VectorULong& b) const {
+        if (size != b.size) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (a[i] != b.a[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator!=(const VectorULong& b) const {
+        return !(*this == b);
+    }
+
+    bool operator>(const VectorULong& b) const {
+        if (size != b.size) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (a[i] <= b.a[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator>=(const VectorULong& b) const {
+        return *this > b || *this == b;
+    }
+
+    bool operator<(const VectorULong& b) const {
+        return !(*this >= b);
+    }
+
+    bool operator<=(const VectorULong& b) const {
+        return !(*this > b);
+    }
+
+    unsigned long& operator[](int index) {
+        if (index < 0 || index >= size) {
+            state = INDEX_OUT_OF_RANGE;
+            return a[size - 1];
+        }
+        return a[index];
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const VectorULong& vec) {
+        for (int i = 0; i < vec.size; ++i) {
+            os << vec.a[i] << ' ';
+        }
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, VectorULong& vec) {
+        for (int i = 0; i < vec.size; ++i) {
+            is >> vec.a[i];
+        }
+        return is;
+    }
+};
+
+int VectorULong::objectCount = 0;
+
+int main() {
+    VectorULong v1(5, 7);
+    VectorULong v2(5, 15);
+    VectorULong v3(5, 10);
+    VectorULong v4(5, 0);
+
+    std::cout << "v1: " << v1 << std::endl;
+    std::cout << "v2: " << v2 << std::endl;
+    std::cout << "v3: " << v3 << std::endl;
+    std::cout << "v4: " << v4 << std::endl;
+
+    v4 = v2 + v3;
+    std::cout << "v1 = v2 + v3: " << v4 << std::endl;
+    v4 = v2 - v3;
+    std::cout << "v1 = v2 - v3: " << v4 << std::endl;
+    v4 = v2 * 2;
+    std::cout << "v1 = v2 * 2: " << v4 << std::endl;
+    v4 = v2 / 2;
+    std::cout << "v1 = v2 / 2: " << v4 << std::endl;
+    v4 = v2 % 3;
+    std::cout << "v1 = v2 % 3: " << v4 << std::endl;
+    v4 = v2 | v3;
+    std::cout << "v1 = v2 | v3: " << v4 << std::endl;
+    v4 = v2 ^ v3;
+    std::cout << "v1 = v2 ^ v3: " << v4 << std::endl;
+    v4 = v2 & v3;
+    std::cout << "v1 = v2 & v3: " << v4 << std::endl;
+
+    ++v1;
+    std::cout << "++v1: " << v1 << std::endl;
+    v1++;
+    std::cout << "v1++: " << v1 << std::endl;
+    --v1;
+    std::cout << "--v1: " << v1 << std::endl;
+    v1--;
+    std::cout << "v1--: " << v1 << std::endl;
+    return 0;
 }
